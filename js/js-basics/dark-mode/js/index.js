4c5227c312ccaaedcd92fc705ca99c75aad436bc
stepris <122841614+stepris@users.noreply.github.com>
2024-06-04 11:45:53 +0200

    Solve js-basics challenge dark-mode

--- js/js-basics/dark-mode/js/index.js
+++ js/js-basics/dark-mode/js/index.js
@@ -1,3 +1,27 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+// bodyElement.classList.toggle("dark");
+
+const buttonDarkMode = document.querySelector('[data-js="dark-mode-button"]');
+
+const buttonLightMode = document.querySelector('[data-js="light-mode-button"]');
+
+const buttonToggleMode = document.querySelector('[data-js="toggle-button"]');
+
+console.log(buttonDarkMode);
+console.log(buttonLightMode);
+console.log(buttonToggleMode);
+
+buttonDarkMode.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+buttonLightMode.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+buttonToggleMode.addEventListener("click", () => {
+  bodyElement.classList.toggle("dark");
+});
