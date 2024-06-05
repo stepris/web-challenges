c5457630466943506d7a8716cabec56116dbb0f5
stepris <122841614+stepris@users.noreply.github.com>
2024-06-05 11:23:14 +0200

    Add and solve js-variables-and-numbers callenges

---
+++ js/js-variables-and-numbers/media-post/js/index.js
@@ -0,0 +1,30 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+
+const title = undefined;
+const text = undefined;
+const likesNumber = 0;
+const postCreator = undefined;
+const isReported = undefined;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+
+// --v-- write your code here --v--
+
+console.log(title, text, likesNumber, postCreator, isReported);
+
+// --^-- write your code here --^--
