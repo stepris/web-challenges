ae2d9b6b7856df5593483b20d0f46e2c72589d23
stepris <122841614+stepris@users.noreply.github.com>
2024-06-10 14:24:02 +0200

    Add js-inputs-and-strings challenges

---
+++ js/js-inputs-and-strings/multiple-inputs/js/index.js
@@ -0,0 +1,44 @@
+/* Sub-Challenge 1
+Implement the following functionality: The value of the first input field is copied into the second input field on button click */
+
+const firstCopyInput = document.querySelector("[data-js=first-copy-input]");
+const secondCopyInput = document.querySelector("[data-js=second-copy-input]");
+const copyButton = document.querySelector("[data-js=copy-button]");
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/* Sub-Challenge 2
+Implement the following functionality: The value of the first input field is copied in uppercase into the second input field on button click */
+
+const firstCopyInUppercaseInput = document.querySelector(
+  "[data-js=first-copy-uppercase-input]"
+);
+const secondCopyInUppercaseInput = document.querySelector(
+  "[data-js=second-copy-uppercase-input]"
+);
+const copyInUppercaseButton = document.querySelector(
+  "[data-js=copy-uppercase-button]"
+);
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
+
+/* Sub-Challenge 3
+Implement the following functionality: The values of the two input fields switch on button click */
+
+const firstSwitchValueInput = document.querySelector(
+  "[data-js=first-switch-value-input]"
+);
+const secondSwitchValueInput = document.querySelector(
+  "[data-js=second-switch-value-input]"
+);
+const switchValueButton = document.querySelector(
+  "[data-js=switch-value-button]"
+);
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
