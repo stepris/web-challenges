763f0b2e4131beb62e384741b00428333535d91b
stepris <122841614+stepris@users.noreply.github.com>
2024-06-11 22:55:34 +0200

    Solve js-forms challenge 3 calculator

--- js/js-forms/calculator/js/index.js
+++ js/js-forms/calculator/js/index.js
@@ -26,7 +26,43 @@ form.addEventListener("submit", (event) => {
 
   // --v-- write your code here --v--
 
+  //console.log(event.target.elements.operator.value);
+
+  const numberA = Number(event.target.elements.numberA.value);
+  const numberB = Number(event.target.elements.numberB.value);
+
+  switch (event.target.elements.operator.value) {
+    case "addition":
+      result = add(numberA, numberB);
+      break;
+    case "subtraction":
+      result = subtract(numberA, numberB);
+      break;
+    case "multiplication":
+      result = multiply(numberA, numberB);
+      break;
+    case "division":
+      result = divide(numberA, numberB);
+      break;
+    default:
+      console.log("Default case nur!");
+  }
+
   // --^-- write your code here --^--
 
   resultOutput.textContent = result;
 });
+
+/* 
+
+Build a calculator that can add, subtract, multiply and divide two numbers.
+
+Add the following functionality to the submit event handler:
+
+Use the values of the numberA and numberB inputs to perform the operation selected by the operator input on them. You can use the provided helper functions add(), subtract(), multiply() and divide().
+Assign the result to the result variable.
+💡 Make sure to thoroughly test the addition operation, as it can have unexpected results.
+
+💡 You only have to touch the js/index.js file.
+
+*/
