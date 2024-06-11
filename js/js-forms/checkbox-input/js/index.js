fdb9e66b9031c1a0b0ec940fc163d32a07fcc526
stepris <122841614+stepris@users.noreply.github.com>
2024-06-11 20:03:12 +0200

    Play through js-forms challenge 2

--- js/js-forms/checkbox-input/js/index.js
+++ js/js-forms/checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const complaintSuccessMessage = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,62 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function hideComplaintSuccess() {
+  complaintSuccessMessage.setAttribute("hidden", "");
+}
+
+function showComplaintSuccess() {
+  complaintSuccessMessage.removeAttribute("hidden");
+}
+
+hideTosError();
+hideComplaintSuccess();
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // --v-- write your code here --v--
+  // Wenn man die Checkbox direkt aus dem DOM greifen will
 
-  // --^-- write your code here --^--
+  /* 
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    return;
+  }
+ */
+
+  // Alternativ auch über per Event-Objekt
+
+  /*
+  if (event.target.elements.tos.checked === false) {
+    // showTosError();
+    // return;
+  }
+ */
+
+  // Dritte Alternative über das FormData-Objekt, das die Formdaten quasi aufbereitet
+
+  // Man kann den value der Checkbox doch in der Form benutzen, dass man überprüft, ob im Event-Objekt die Checkbox mitaufgeführt ist. Der Wert ist zwar immer "on", jedoch wird sie nur mitaufgeführt bei den Properties, wenn sie tatsächlich angeklickt wurde.
+
+  const receivedFormData = new FormData(event.target);
+  const receivedFormDataObject = Object.fromEntries(receivedFormData);
+  // console.log(receivedFormDataObject.tos === "on");
+  if (receivedFormDataObject.tos !== "on") {
+    console.log("ToS nicht gechecked!");
+    showTosError();
+    return;
+  }
+
+  hideTosError();
+  showComplaintSuccess();
 
   // eslint-disable-next-line no-alert
   alert("Form submitted");
+});
+
+tosCheckbox.addEventListener("input", () => {
+  if (tosCheckbox.checked === false) {
+    showTosError();
+    return;
+  }
+  hideTosError();
 });
